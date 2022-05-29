371526e2b8117effcfdeee1b82225cc05d37d26a
Ivan <vanyaboguslavskii@gmail.com>
2022-05-29 22:03:53 +0300

    Создал Пропсы, добавил папку компонентов для разбивки кода на части

--- src/App.js
+++ src/App.js
@@ -2,12 +2,34 @@ import Card from './components/Card';
 import Header from './components/Header';
 import Drawer from './components/Drawer';
 
+const arr = [
+  {
+    title: 'Мужские Кроссовки Nike Blazer Mid Suede',
+    price: '12 999',
+    imageUrl: '/img/sneakers/1.jpg',
+  },
+  {
+    title: 'Мужские Кроссовки Nike Air Max 270',
+    price: '13 999',
+    imageUrl: '/img/sneakers/2.jpg',
+  },
+  {
+    title: 'Мужские Кроссовки Nike Blazer Mid Suede',
+    price: '8 499',
+    imageUrl: '/img/sneakers/3.jpg',
+  },
+  {
+    title: 'Кроссовки Puma X Aka Boku Future Rider',
+    price: '8 999',
+    imageUrl: '/img/sneakers/4.jpg',
+  },
+];
+
 function App() {
   return (
     <div className='wrapper clear'>
       <Drawer />
       <Header />
-
       <div className='content p-40'>
         <div className='d-flex align-center justify-between mb-40'>
           <h1>Все кроссовки</h1>
@@ -16,11 +38,15 @@ function App() {
             <input placeholder='Поиск...' />
           </div>
         </div>
-        <div className='d-flex flex-wrap justify-end'>
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+        <div className='d-flex flex-wrap'>
+          {arr.map((obj) => (
+            <Card
+              title={obj.title}
+              price={obj.price}
+              imageUrl={obj.imageUrl}
+              onClick={() => console.log(obj)}
+            />
+          ))}
         </div>
       </div>
     </div>
